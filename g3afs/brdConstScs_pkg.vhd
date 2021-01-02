a946df9c9dc2e45f2a4c62562071c54a4cf311de
Anton Mause <AntonMause@users.noreply.github.com>
2021-01-02 15:14:05 +0100

    Copy polarity handling from rv16poc to tcl4soc
    Signed-off-by: Anton Mause <AntonMause@users.noreply.github.com>

--- g3afs/brdConstScs_pkg.vhd
+++ g3afs/brdConstScs_pkg.vhd
@@ -6,6 +6,15 @@
 --
 -- Package to declare board specific constants.
 --
+-- LEDs & PushButton SW polarity XOR constants
+-- Handling examples :
+--   constant c_lex : std_logic := BRD_LED_POL;
+--   constant c_pbx : std_logic := BRD_BTN_POL;
+--
+--   LED0   <=  c_lex xor s_led(0);
+--   LED2   <=  c_lex;         -- force idle LEDs OFF on all boards
+--   s_pb1  <=  c_pbx xor PB1; -- force '1' only if pressed
+--
 ----------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -13,6 +22,8 @@ use ieee.std_logic_1164.all;
 ----------------------------------------------------------------------
 package brdConst_pkg is
   constant BRD_OSC_CLK_MHZ : positive;
+  constant BRD_LED_POL : std_logic;
+  constant BRD_BTN_POL : std_logic;
 end brdConst_pkg;
 
 ----------------------------------------------------------------------
@@ -21,6 +32,17 @@ package body brdConst_pkg is
 --constant BRD_OSC_CLK_MHZ : positive := 50_000_000; -- OSC divided
   constant BRD_OSC_CLK_MHZ : positive := 48_000_000; -- XTL direct
 --constant BRD_OSC_CLK_MHZ : positive := 24_000_000; -- XTL divided
+
+  -- polarity of LED driver output
+  -- '0' = low idle, high active
+  -- '1' = high idle, low active
+  constant BRD_LED_POL : std_logic := '1';
+
+  -- polarity of push button switches
+  -- '0' = low idle, high active (pressed)
+  -- '1' = high idle, low active (pressed)
+  constant BRD_BTN_POL : std_logic := '0';
+
 end brdConst_pkg;
 
 ----------------------------------------------------------------------
